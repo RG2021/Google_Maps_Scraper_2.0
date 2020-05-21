71c85b8a9944571d91323db01f8fd581cdb71eb1
Rohan Goel <rohangoel501@gmail.com>
2020-05-21 14:46:25 +0530

    Added step 2 for scraping

--- scrape.py
+++ scrape.py
@@ -6,7 +6,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.action_chains import ActionChains
 import time
 
-data = ["ChIJZ-DLDJ9HrTsR681u8-Ui8v4", "ChIJtV8FufYdDTkRwKMeayYLwYE"]
+data = ["ChIJZ-DLDJ9HrTsR681u8-Ui8v4"]
 
 PATH = "C:\Program Files (x86)\chromedriver.exe"
 
@@ -40,13 +40,71 @@ for place_id in data:
 	location_data[place_id]["location"] = address.text
 	location_data[place_id]["contact"] = phone_number.text
 
-	location_data[place_id]["Time"] = {}
-
 	day = [a.text for a in days]
 	open_close_time = [a.text for a in times]
 
+	location_data[place_id]["Time"] = {}
+
 	for i, j in zip(day, open_close_time):
 		location_data[place_id]["Time"][i] = j
+
+
+	try:
+		# waits until the button is loaded into the HTML
+		WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.CLASS_NAME, "allxGeDnJMl__button")))
+
+		element = driver.find_element_by_class_name("allxGeDnJMl__button")
+		element.click()
+	except:
+		driver.quit()
+
+
+	try:
+
+		WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "section-layout-root")))
+
+		pause_time = 2
+		max_count = 5
+		x = 0
+
+		while(x<max_count):
+
+			scrollable_div = driver.find_element_by_css_selector('div.section-layout.section-scrollbox.scrollable-y.scrollable-show')
+
+			driver.execute_script('arguments[0].scrollTop = arguments[0].scrollHeight', scrollable_div)
+			time.sleep(pause_time)
+
+			x=x+1
+
+	except:
+		driver.quit()
+
+
+	element = driver.find_elements_by_class_name("section-expand-review")
+	for i in element:
+		i.click()
+
+
+	review_names = driver.find_elements_by_class_name("section-review-title")
+	review_text = driver.find_elements_by_class_name("section-review-review-content")
+	review_dates = driver.find_elements_by_css_selector("[class='section-review-publish-date']")
+	review_stars = driver.find_elements_by_css_selector("[class='section-review-stars']")
+
+	review_stars_final = []
+
+	for i in review_stars:
+		review_stars_final.append(i.get_attribute("aria-label"))
+
+	review_names_list = [a.text for a in review_names]
+	review_text_list = [a.text for a in review_text]
+	review_dates_list = [a.text for a in review_dates]
+	review_stars_list = [a for a in review_stars_final]
+
+	location_data[place_id]["Reviews"] = []
+
+	for (a,b,c,d) in zip(review_names_list, review_text_list, review_dates_list, review_stars_list):
+		location_data[place_id]["Reviews"].append({"name":a, "review":b, "date":c, "rating":d})
+
 
 	driver.quit()
 
